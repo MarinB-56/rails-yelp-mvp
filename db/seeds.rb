a5176ad15e44240e6c8cb97cd0df98d0839dd480
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-09-24 17:17:58 +0200

    Created the seed file to create 5 random restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+CATEGORIES = %w[chinese italian japanese french belgian]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: CATEGORIES.sample
+  )
+  puts "Created #{restaurant.name}"
+  restaurant.save
+end
+
+# Restaurant name
+# Restaurant address
+# Restaurant rating
+# Restaurant
